cc8c15bd80e4371bb2c17e1f0d5ef47dfb24059b
Josh Mahowald <mahowald@gmail.com>
2016-08-22 14:44:42 -0500

    Added in variable calls, makefile to build, execution script to run

--- one-host.tf
+++ one-host.tf
@@ -1,21 +1,37 @@
+
+variable "aws_region"{}
+# TODO, this should be a list
+variable "aws_availability_zone" {}
+variable "owner" {}
+variable "environment_name"{}
+variable "ssh_keypath" {}
+variable "key_name" {}
+variable "public_subnet_id" {}
+variable "security_group_ids" {
+  type = "list"
+}
+
+
 module "coreos_amis" {
   source = "github.com/terraform-community-modules/tf_aws_coreos_ami"
   region = "${var.aws_region}"
   channel = "stable"
   virttype = "hvm"
 }
+
+
 resource "aws_instance" "registry" {
   #  availability_zone = "${var.aws_availability_zone}"
     ami = "${module.coreos_amis.ami_id}"
-    subnet_id = "${terraform_remote_state.vpc.output.public_subnet_id}"
+    subnet_id = "${var.public_subnet_id}"
     instance_type = "t2.micro"
-    key_name = "${terraform_remote_state.vpc.output.key_name}"
+    key_name = "${var.key_name}"
     # Very very weird that I can't use the vpc output for this and have to
     # supply it as a variable. Plus even when I do, it only allows two
-    vpc_security_group_ids = ["${terraform_remote_state.vpc.output.security_group_ids}",
+    vpc_security_group_ids = ["${var.security_group_ids}",
       "${aws_security_group.docker_registry.id}",
-      "${terraform_remote_state.vpc.output.nat_security_group}",
-      "${terraform_remote_state.vpc.output.web_security_group}"]
+      "${var.nat_security_group}",
+      "${var.web_security_group}"]
     tags {
       Name = "registry"
       Owner = "${var.owner}"
@@ -23,10 +39,7 @@ resource "aws_instance" "registry" {
 
     connection {
         user =  "core"
-        key_file = "${terraform_remote_state.vpc.output.key_file}"
-        /*bastion_host = "${terraform_remote_state.vpc.output.bastion_ip}"
-        bastion_user = "${terraform_remote_state.vpc.output.bastion_user}"
-        bastion_private_key = "${file(terraform_remote_state.vpc.output.key_file)}" å*/
+        key_file = "${var.key_file}"
     }
 
     provisioner "remote-exec" {
